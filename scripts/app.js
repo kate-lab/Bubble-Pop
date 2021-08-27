df9a8a5b397673c44052d308c74eb99d0e623907
Kate Westbrook <katewestbrook@live.co.uk>
2021-08-27 17:08:32 +0100

    added bullet to character

--- scripts/app.js
+++ scripts/app.js
@@ -15,12 +15,15 @@ function init() {
   // enemy variables
   const enemyClassName = 'enemy'
   const enemyStartingPosition = 0
-  // let enemyCurrentPosition = enemyStartingPosition
+  let enemyCurrentPosition = enemyStartingPosition
 
   //character variables
   const charClassName = 'character'
   const charStartingPosition = (width * width) - parseFloat(width / 2)
   let charCurrentPosition = charStartingPosition
+
+  //character bullet variables
+  const charBulletClassName = 'charBullet'
 
   //execution
 
@@ -43,6 +46,9 @@ function init() {
     addEnemy(enemyStartingPosition)
   }
 
+
+
+
   //function to add character to cell
   function addCharacter(cellPosition){
     cells[cellPosition].classList.add(charClassName)
@@ -52,19 +58,69 @@ function init() {
     cells[position].classList.remove(charClassName)
   }
 
+  //add character's bullet
+  function addCharBullet(cellPosition){
+    cells[cellPosition].classList.add(charBulletClassName)
+  }
+  //remove character's bullet (use this when running setinterval to auto move bullet)
+  function removeCharBullet(position){
+    cells[position].classList.remove(charBulletClassName)
+  }
+
+  //function that shoots character bullet
+  function shootCharBullet(){
+    //variables that show position of bullet in cell above char
+    const charBulletStartingPosition = charCurrentPosition - 10
+    let charBulletCurrentPosition = charBulletStartingPosition
+
+    // function to add bullet in this place
+    addCharBullet(charBulletStartingPosition)
+
+
+    //function to begin automatic movement of bullet
+    const moveCharBullet = setInterval(()=>{
+
+      if  (charBulletCurrentPosition < 0){ //if bullet goes past grid, remove and clear interval
+        removeCharBullet(charBulletCurrentPosition)
+        clearInterval(moveCharBullet) 
+      } else if (charBulletCurrentPosition !== enemyCurrentPosition) { //if the bullet is not in the same position the enemy, move upwards
+        removeCharBullet(charBulletCurrentPosition)
+        charBulletCurrentPosition -= 10
+        addCharBullet(charBulletCurrentPosition)
+      }
+    }, 100)
+  
+    // if bullet and enemy are in same position, remove both!
+    function boomChecker(charBulletCurrentPosition){
+      if (charBulletCurrentPosition === enemyCurrentPosition){
+        removeEnemy(enemyCurrentPosition)
+        removeCharBullet(charBulletCurrentPosition)
+      } else {
+        moveCharBullet
+      }
+    }
+
+    boomChecker()
+
+
+  }
+
+
+
   //add enemy
   function addEnemy(cellPosition){
     cells[cellPosition].classList.add(enemyClassName)
   }
-  // //remove enemy
-  // function removeEnemy(position){
-  //   cells[position].classList.remove(enemyClassName)
-  // }
+  //remove enemy
+  function removeEnemy(position){
+    cells[position].classList.remove(enemyClassName)
+  }
+
+
 
   //user key input function - move character left right and shoot
   function handleKeyDown (event){
     removeCharacter(charCurrentPosition)
-    console.log(event.code)
 
     const key = event.code
     const right = 'ArrowRight'
@@ -77,10 +133,16 @@ function init() {
       charCurrentPosition--
     } else if (key === space){
       console.log('I am shooting!')
+      shootCharBullet()
     }
     
     addCharacter(charCurrentPosition)
   }
+
+  
+  
+
+
   //creates grid and adds character
   startButton.addEventListener('click', createGrid)
   //handles keydown of character and moves character to desired place
