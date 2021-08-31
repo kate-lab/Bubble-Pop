7cd94dd5ed0a0bf2e07ef61807d0c38f037b71ea
Kate Westbrook <katewestbrook@live.co.uk>
2021-08-31 21:43:29 +0100

    added multiple enemies

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@ function init() {
   const startMenu = document.querySelector('#start-menu')
   const startButton = document.querySelector('#start-button')
   const startCounter = document.querySelector('#start-count')
+  
 
 
   // grid variables
@@ -13,30 +14,53 @@ function init() {
   const cells = []
   const cellCount = width * width
 
-
   //on page button variables
   const right = document.querySelector('#right-button')
   const left = document.querySelector('#left-button')
   const space = document.querySelector('#space-button')
 
 
+  //score variables
+  const scoreHolder = document.getElementById('point-container')
+  let currentScore = 0
+  scoreHolder.innerText = currentScore
+  // const userTopScore = localStorage.getItem() // for each score in score[] parsefloat(score)
+
+  //user variables
+  const inputUserName = document.getElementById('name')
+  
+
+  //lives variable
+  const livesHolder = document.getElementById('lives-container')
+  const currentLives = 3 //change to let when you add takeLife() function
+  livesHolder.innerText = currentLives
+
+
   // enemy variables
+
   const enemyClassName = 'enemy'
+  const enemies = []
+
   const enemyStartingPosition = 0
   let enemyCurrentPosition = enemyStartingPosition
+
+  const enemiesArrayLength = parseInt(width * 0.7)
 
   //enemy bullet variables
 
 
   //character variables
-  const charStartingClass = ''
-  let chosenCharClass = charStartingClass
+  let chosenCharClass = ''
+  const charStartingPosition = (width * width) - parseFloat(width / 2)
+  let charCurrentPosition = charStartingPosition
   
   //character selector variables
   const selectCharMenu = document.querySelector('#character-select')
   
+  //character bullet variables
+  const charBulletClassName = 'charBullet'
 
-  //function to update image container with selected class
+  //function to update image container with selected class and add corresponding character to grid
   function updateChar(event){
     chosenCharClass = event.target.value
     if (chosenCharClass === 'triangle'){
@@ -46,16 +70,31 @@ function init() {
     } else if (chosenCharClass === 'square'){
       document.getElementById('character-img').src = 'assets/images/characters/square.png'
     }
+    
+    // start button enabler based on selection
+    if (inputUserName.value !== '' || chosenCharClass !== ''){
+      startButton.disabled = false
+    } else {
+      startButton.disabled = true
+    }
   }
 
-  const charStartingPosition = (width * width) - parseFloat(width / 2)
-  let charCurrentPosition = charStartingPosition
-
-  //character bullet variables
-  const charBulletClassName = 'charBullet'
 
 
   //execution
+
+  //storing user details
+
+  function storeUserDetails(){
+    //stores username
+    localStorage.setItem('name', inputUserName.value)
+    //adds user name to current-user span
+    const currentUserName = localStorage.getItem('name')
+    document.querySelector('#current-user').innerHTML = currentUserName
+  }
+
+  
+
 
   //LAYOUT SET UP
 
@@ -72,10 +111,14 @@ function init() {
     //calling function to add character on grid
     addCharacter(charStartingPosition)
     
-    //calling function to add enemy on grid and make it start moving after timeout delay
+    //calling function to add enemies on grid and make it start moving after timeout delay
     setTimeout(()=>{
-      //need to add enemies - can I add const enemies = [] for each cell 0-6, 10-16 & 20-26
-      addEnemy(enemyStartingPosition)
+
+      // addEnemies
+      for (let i = 0; i < enemiesArrayLength; i++) {
+        cells[i].classList.add(enemyClassName)
+      }
+      
       //autoMoveEnemy
     }, 4000)
 
@@ -92,184 +135,193 @@ function init() {
         }, 1000)
       }
     }, 1000)
-  }
-
-  //CHARACTER
 
 
-  //function to add character to cell
-  function addCharacter(cellPosition){
-    cells[cellPosition].classList.add(chosenCharClass)
-  }
-  //function to remove character from cell
-  function removeCharacter(position){
-    cells[position].classList.remove(chosenCharClass)
-  }
-
-  //add character's bullet
-  function addCharBullet(cellPosition){
-    cells[cellPosition].classList.add(charBulletClassName)
-  }
-  //remove character's bullet (use this when running setinterval to auto move bullet)
-  function removeCharBullet(position){
-    cells[position].classList.remove(charBulletClassName)
-  }
-
-  //function that shoots character bullet
-  function shootCharBullet(){
-    //variables that show position of bullet in cell above char
-    const charBulletStartingPosition = charCurrentPosition - 10
-    let charBulletCurrentPosition = charBulletStartingPosition
-
-    // function to add bullet in this place
-    addCharBullet(charBulletStartingPosition)
-
-    //function to begin automatic movement of bullet & define outcomes if bullet hits enemy or bullet goes past grid
-    const moveBullet = setInterval(()=>{
-      if (charBulletCurrentPosition === charCurrentPosition - (cellCount - width)){ //if bullet gets to end of grid, remove and clear interval
-        removeEnemy(charBulletCurrentPosition)
-        removeCharBullet(charBulletCurrentPosition)
-        clearInterval(moveBullet)
-      } else if (charBulletCurrentPosition !== enemyCurrentPosition) { //if the bullet is not in the same position the enemy, move upwards
-        removeCharBullet(charBulletCurrentPosition)
-        charBulletCurrentPosition -= 10
-        addCharBullet(charBulletCurrentPosition)
-      } else if (charBulletCurrentPosition === enemyCurrentPosition) {  // if bullet and enemy are in same position, remove both!
-        removeEnemy(enemyCurrentPosition)
-        removeCharBullet(charBulletCurrentPosition)
-        clearInterval(moveBullet)
-      } // do i need to set a new else statement for when there are no enemies left?
-    }, 100)
-  }
+    //CHARACTER
 
 
-
-  
-
-  //ENEMIES
-
-
-
-  //add enemy
-  function addEnemy(cellPosition){
-    cells[cellPosition].classList.add(enemyClassName)
-  }
-  //remove enemy
-  function removeEnemy(position){
-    cells[position].classList.remove(enemyClassName)
-  }
-
-  // //auto move the enemy every second
-  // const autoMoveEnemy = setInterval(()=>{
-  //   if (enemyCurrentPosition % width !== width - 1){
-  //     moveEnemyRight
-  //   } else if (enemyCurrentPosition % width === width - 1){
-  //     moveEnemyDown
-  //   }
-  // }, 1000)
-  
-  // // function to move enemy 1 cell to right
-  // function moveEnemyRight(){
-  //   removeEnemy(enemyCurrentPosition)
-  //   enemyCurrentPosition++
-  //   addEnemy(enemyCurrentPosition)
-  // }
-
-  // function moveEnemyDown(){
-  //   removeEnemy(enemyCurrentPosition)
-  //   enemyCurrentPosition += 10
-  //   addEnemy(enemyCurrentPosition)
-  // }
-
-  // function moveEnemyLeft(){
-  //   removeEnemy(enemyCurrentPosition)
-  //   enemyCurrentPosition--
-  //   addEnemy(enemyCurrentPosition)
-  // }
-    
-
-
-  // USER INPUT - CHARACTER CHOICE
-
-  // function charSelector
-
-
-  // USER INPUT - GAMEPLAY
-
-  //user key input function - move character left right and shoot
-  function handleKeyDown (event){
-    removeCharacter(charCurrentPosition)
-
-    const key = event.code
-    const right = 'ArrowRight'
-    const left = 'ArrowLeft'
-    const space = 'Space'
-
-    if (key === right && charCurrentPosition % width !== width - 1){
-      charCurrentPosition++
-    } else if (key === left && charCurrentPosition % width !== 0){
-      charCurrentPosition--
-    } else if (key === space){
-      console.log('I am shooting!')
-      shootCharBullet()
+    //function to add character to cell
+    function addCharacter(cellPosition){
+      cells[cellPosition].classList.add(chosenCharClass)
     }
+    //function to remove character from cell
+    function removeCharacter(position){
+      cells[position].classList.remove(chosenCharClass)
+    }
+
+    //add character's bullet
+    function addCharBullet(cellPosition){
+      cells[cellPosition].classList.add(charBulletClassName)
+    }
+    //remove character's bullet (use this when running setinterval to auto move bullet)
+    function removeCharBullet(position){
+      cells[position].classList.remove(charBulletClassName)
+    }
+
+    function addPoints(){
+      scoreHolder.innerText = currentScore += 100
+    }
+
+    //function that shoots character bullet
+    function shootCharBullet(){
+      //variables that show position of bullet in cell above char
+      const charBulletStartingPosition = charCurrentPosition - 10
+      let charBulletCurrentPosition = charBulletStartingPosition
+
+      // function to add bullet in this place
+      addCharBullet(charBulletStartingPosition)
+
+
+      // can I use? charBulletCurrentPosition.classList.contains(enemyClassName)
+      //function to begin automatic movement of bullet & define outcomes if bullet hits enemy or bullet goes past grid
+      const moveBullet = setInterval(()=>{
+        if (charBulletCurrentPosition === enemyCurrentPosition) { 
+          // if bullet and enemy are in same position, remove both!
+          removeCharBullet(charBulletCurrentPosition)
+          removeEnemy(enemyCurrentPosition)
+          console.log('Popped!')
+          addPoints()
+          clearInterval(moveBullet)
+        } else if (charBulletCurrentPosition !== enemyCurrentPosition && charBulletCurrentPosition >= 10) { //if the bullet is not in the same position the enemy, move upwards
+          removeCharBullet(charBulletCurrentPosition)
+          charBulletCurrentPosition -= 10
+          addCharBullet(charBulletCurrentPosition)
+        } else { //if bullet gets to end of grid, remove and clear interval
+          console.log('bullet wasted!')
+          removeEnemy(charBulletCurrentPosition)
+          removeCharBullet(charBulletCurrentPosition)
+          clearInterval(moveBullet)
+        } 
+        // do i need to set a new else statement for when there are no enemies left?
+      }, 100)
+    }
+
+
+    //add enemy
+
+    function addEnemy(positions){
+      cells[positions].classList.add(enemyClassName)
+    }
+    //remove enemy
+    function removeEnemy(position){
+      cells[position].classList.remove(enemyClassName)
+    }
+
+    // //auto move the enemy every second
+    // const autoMoveEnemy = setInterval(()=>{
+    //   if (enemyCurrentPosition % width !== width - 1){
+    //     moveEnemyRight
+    //   } else if (enemyCurrentPosition % width === width - 1){
+    //     moveEnemyDown
+    //   }
+    // }, 1000)
     
-    addCharacter(charCurrentPosition)
+    // // function to move enemy 1 cell to right
+    // function moveEnemyRight(){
+    //   removeEnemy(enemyCurrentPosition)
+    //   enemyCurrentPosition++
+    //   addEnemy(enemyCurrentPosition)
+    // }
+
+    // function moveEnemyDown(){
+    //   removeEnemy(enemyCurrentPosition)
+    //   enemyCurrentPosition += 10
+    //   addEnemy(enemyCurrentPosition)
+    // }
+
+    // function moveEnemyLeft(){
+    //   removeEnemy(enemyCurrentPosition)
+    //   enemyCurrentPosition--
+    //   addEnemy(enemyCurrentPosition)
+    // }
+
+
+    // USER INPUT - GAMEPLAY
+
+    //user key input function - move character left right and shoot
+    function handleKeyDown (event){
+      removeCharacter(charCurrentPosition)
+
+      const key = event.code
+      const right = 'ArrowRight'
+      const left = 'ArrowLeft'
+      const space = 'Space'
+
+      if (key === right && charCurrentPosition % width !== width - 1){
+        charCurrentPosition++
+      } else if (key === left && charCurrentPosition % width !== 0){
+        charCurrentPosition--
+      } else if (key === space){
+        console.log('I am shooting!')
+        shootCharBullet()
+      }
+      
+      addCharacter(charCurrentPosition)
+    }
+
+    //user click button on page function
+
+    // left
+    function handleLeftClick (event){
+      //ensures img is also included in click
+      const leftArrow = document.querySelector('#left-arrow')
+      removeCharacter(charCurrentPosition)
+      if (event.target === left && charCurrentPosition % width !== 0){
+        charCurrentPosition--
+      } else if (event.target === leftArrow && charCurrentPosition % width !== 0){
+        charCurrentPosition--
+      } 
+      addCharacter(charCurrentPosition)
+    }
+
+    //right
+    function handleRightClick (event){
+      //ensures img is also included in click
+      const rightArrow = document.querySelector('#right-arrow')
+      removeCharacter(charCurrentPosition)
+      if (event.target === right && charCurrentPosition % width !== width - 1){
+        charCurrentPosition++
+      } else if (event.target === rightArrow && charCurrentPosition % width !== width - 1){
+        charCurrentPosition++
+      } 
+      addCharacter(charCurrentPosition)
+    }
+
+    //space
+    function handleSpaceClick (event){
+      removeCharacter(charCurrentPosition)
+      if (event.target === space){
+        console.log('I am shooting!')
+        shootCharBullet()
+      }  
+      addCharacter(charCurrentPosition)
+    }
+
+    //EVENT LISTENERS WHEN GRID IS INITIALISED
+
+    //handles keydown of character and moves character to desired place
+    document.addEventListener('keydown', handleKeyDown)
+
+    //handles on page buttons to move character and shoot
+    document.addEventListener('click', handleLeftClick)
+    document.addEventListener('click', handleRightClick)
+    document.addEventListener('click', handleSpaceClick)
+
+
   }
 
-  //user click button on page function
-
-  // left
-  function handleLeftClick (event){
-    //ensures img is also included in click
-    const leftArrow = document.querySelector('#left-arrow')
-    removeCharacter(charCurrentPosition)
-    if (event.target === left && charCurrentPosition % width !== 0){
-      charCurrentPosition--
-    } else if (event.target === leftArrow && charCurrentPosition % width !== 0){
-      charCurrentPosition--
-    } 
-    addCharacter(charCurrentPosition)
-  }
-
-  //right
-  function handleRightClick (event){
-    //ensures img is also included in click
-    const rightArrow = document.querySelector('#right-arrow')
-    removeCharacter(charCurrentPosition)
-    if (event.target === right && charCurrentPosition % width !== width - 1){
-      charCurrentPosition++
-    } else if (event.target === rightArrow && charCurrentPosition % width !== width - 1){
-      charCurrentPosition++
-    } 
-    addCharacter(charCurrentPosition)
-  }
-
-  //space
-  function handleSpaceClick (event){
-    removeCharacter(charCurrentPosition)
-    if (event.target === space){
-      console.log('I am shooting!')
-      shootCharBullet()
-    }  
-    addCharacter(charCurrentPosition)
-  }
-
-
-  //EVENT LISTENERS
+  //EVENT LISTENERS AFTER INIT
 
   //changes character picture on change of selector options
   selectCharMenu.addEventListener('change', updateChar)
 
+  //saves username on startbutton click
+  startButton.addEventListener('click', storeUserDetails)
+
   //creates grid and adds character
   startButton.addEventListener('click', createGrid)
-  //handles keydown of character and moves character to desired place
-  document.addEventListener('keydown', handleKeyDown)
 
-  //handles on page buttons to move character and shoot
-  document.addEventListener('click', handleLeftClick)
-  document.addEventListener('click', handleRightClick)
-  document.addEventListener('click', handleSpaceClick)
+
 
 }
 window.addEventListener('DOMContentLoaded', init)
